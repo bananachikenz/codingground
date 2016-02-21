7ab92996eba5cf0b7feaca3edfa2afb721f54d21
bananachikenz <emea27@inbox.lv>
2016-02-21 09:32:07 +0200

    Created by Coding Ground Client

---
+++ New Project/Bananas.java
@@ -0,0 +1,43 @@
+Class Bananas extends France {
+    
+    int age;
+    
+    public Bananas(int playerAge){
+        age = playerAge;
+    }
+    
+    public void nextGame() {
+        System.out.println("Tonight there was a league game")
+    }
+    
+    public void pointsScored(int pScored) {
+        System.out.println("The seat price will be " + pScored + " euros!")
+    }
+    
+    public int getPlayers() {
+        return players;
+    }
+    
+    public static void main(String[] args) {
+        Player lebron = new Player(31);
+        lebron.pointsScored(48);
+        int 
+    }
+}
+
+
+
+
+
+  
+  
+  
+		public static void main(String[] args) {
+      Dog spike = new Dog(5);
+      spike.bark();
+      spike.run(600000);
+      int spikeAge = spike.getAge();
+      System.out.println(spikeAge);
+	}
+
+}
